0f68ba03bf81ddcf3db765e43d00579f903a0b76
Yamkela24 <151846985+Yamkela24@users.noreply.github.com>
2023-12-08 15:32:18 +0200

    First Commit

---
+++ Assignment3/House.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class House
+    {
+        private int NoOfDoors;
+        private bool HasFurniture;
+        private int HasWindows;
+        private string WhatColourIsIt;
+
+        public House(int noOfDoors, bool hasFurniture, int hasWindows, string whatColourIsIt) 
+        { 
+            NoOfDoors = noOfDoors;
+            HasFurniture = hasFurniture;
+            HasWindows = hasWindows;
+            WhatColourIsIt = whatColourIsIt;
+
+        
+        
+        }
+        public int GetDoors()
+        { 
+             return NoOfDoors; 
+        }
+        public bool GetFurniture()
+        {
+            return HasFurniture;
+        }
+        public int GetWindows()
+        {
+            return HasWindows;
+        }
+        public string GetColour()
+        {
+            return WhatColourIsIt;
+        }
+    }
+    
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            House myHouse = new House(2,true,6,"Pink");
+
+            Console.WriteLine("How many doors? " + myHouse.GetDoors());
+            Console.WriteLine("Does it have furniture? " + myHouse.GetFurniture() );
+            Console.WriteLine("How many windows does it have? " + myHouse.GetWindows() );
+            Console.WriteLine("What colour is it? " + myHouse.GetColour() );
+        }
+    }
+}
